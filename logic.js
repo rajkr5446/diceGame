784a693d09809d4e8923dd6577d6b59dcf1d9102
Raja Kumar <rajkr5446@gmail.com>
2023-01-23 19:04:41 +0530

    Updated logic.ls
    corrected the path for images

--- logic.js
+++ logic.js
@@ -1,11 +1,11 @@
 // for img 1
 var randomNumber1 = Math.floor(Math.random()*6) + 1;
-var imageSrc1 = "/images/dice" + randomNumber1 + ".png";
+var imageSrc1 = "images/dice" + randomNumber1 + ".png";
 document.querySelectorAll("img")[0].setAttribute("src", imageSrc1);
 
 // for img 2
 var randomNumber2 = Math.floor(Math.random()*6) + 1;
-var imageSrc2 = "/images/dice" + randomNumber2 + ".png";
+var imageSrc2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", imageSrc2);
 
 var result;
