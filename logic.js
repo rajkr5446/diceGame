201456b284fdd9a12e8924d350d9634112c3c9e7
Raja Kumar <rajkr5446@gmail.com>
2023-01-23 08:00:35 +0530

    Uploaded Files
    Uploaded all the main files

---
+++ logic.js
@@ -0,0 +1,22 @@
+// for img 1
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var imageSrc1 = "/images/dice" + randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", imageSrc1);
+
+// for img 2
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var imageSrc2 = "/images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", imageSrc2);
+
+var result;
+if(randomNumber1>randomNumber2) {
+    result = "🚩 Player 1 wins!";
+}
+else if(randomNumber1<randomNumber2) {
+    result = "Player 2 wins! 🚩";
+}
+else {
+    result = "Drwa!";
+}
+
+document.querySelector("h1").innerHTML = result;
